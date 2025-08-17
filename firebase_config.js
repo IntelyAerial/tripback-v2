ccb66a5fc38c6534f91f3c40b69745c5a7f9358e
IntelyAerial <info@intely.se>
2025-08-17 22:35:04 +0200

    Update firebase_config.js

--- firebase_config.js
+++ firebase_config.js
@@ -2,13 +2,16 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.3.1/firebas
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAptTU7F2FD5G6ayNWldG1UQlAgycqpoGA",
-    authDomain: "travelmate-2025.firebaseapp.com",
-    projectId: "travelmate-2025",
-    storageBucket: "travelmate-2025.firebasestorage.app",   // ✅ rätt bucket
-    messagingSenderId: "717656692605",
-    appId: "1:717656692605:web:d7ea4a0ca62e68ec885e14"
+  apiKey: "AIzaSyAptTU7F2FD5G6ayNWldG1UQlAgycqpoGA",
+  authDomain: "travelmate-2025.firebaseapp.com",
+  databaseURL: "https://travelmate-2025-default-rtdb.firebaseio.com", // ✅ Lägg till denna rad
+  projectId: "travelmate-2025",
+  storageBucket: "travelmate-2025.firebasestorage.app",
+  messagingSenderId: "717656692605",
+  appId: "1:717656692605:web:d7ea4a0ca62e68ec885e14",
+  measurementId: "G-2R8W7J6DBQ"
 };
+
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
